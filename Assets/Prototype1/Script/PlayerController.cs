027fc0b9981e1c81f291277ae15fde1f8a5e4345
MichaelJusman <107088338+MichaelJusman@users.noreply.github.com>
2023-02-17 16:13:44 +1100

    Enemy movement & spawn

---
+++ Assets/Prototype1/Script/PlayerController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    
+    private Rigidbody rb;
+    public float speed = 5.0f;
+    private GameObject focalPoint;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("FocalPoint");
+    }
+
+    void Update()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+        rb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+    }
+}
