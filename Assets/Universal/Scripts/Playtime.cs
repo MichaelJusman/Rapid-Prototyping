88c5680b69aae2422ebfdd151efd871a8836f0f8
MichaelJusman <107088338+MichaelJusman@users.noreply.github.com>
2023-03-01 12:59:30 +1100

    fix

--- Assets/Universal/Scripts/Playtime.cs
+++ Assets/Universal/Scripts/Playtime.cs
@@ -16,56 +16,56 @@ public class Playtime : GameBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //ExecuteAfterSeconds(2, () => ScaleToZero(player));
+        ExecuteAfterSeconds(2, () => ScaleToZero(player));
 
-        //ExecuteAfterSeconds(2, () =>
-        //{
-        //    ChangePlayerColour();
-        //    print("woo hoo");
-        //});
-        
+        ExecuteAfterSeconds(2, () =>
+        {
+            ChangePlayerColour();
+            print("woo hoo");
+        });
+
     }
 
-    // Update is called once per frame
-    //void Update()
-    //{
-    //    if(Input.GetKeyDown(KeyCode.Space))
-    //    {
-    //        ChangePlayerColour();
-    //    }
+    //Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ChangePlayerColour();
+        }
 
-    //    if (Input.GetKeyDown(KeyCode.D))
-    //        MovePlayer(Direction.Right);
-    //    if (Input.GetKeyDown(KeyCode.A))
-    //        MovePlayer(Direction.Left);
-    //    if (Input.GetKeyDown(KeyCode.W))
-    //        MovePlayer(Direction.Up);
-    //    if (Input.GetKeyDown(KeyCode.S))
-    //        MovePlayer(Direction.Down);
-    //    if (Input.GetKeyDown(KeyCode.R))
-    //        ScalePlayer(Vector3.one);
-    //}
+        if (Input.GetKeyDown(KeyCode.D))
+            MovePlayer(Direction.Right);
+        if (Input.GetKeyDown(KeyCode.A))
+            MovePlayer(Direction.Left);
+        if (Input.GetKeyDown(KeyCode.W))
+            MovePlayer(Direction.Up);
+        if (Input.GetKeyDown(KeyCode.S))
+            MovePlayer(Direction.Down);
+        if (Input.GetKeyDown(KeyCode.R))
+            ScalePlayer(Vector3.one);
+    }
 
-    //void MovePlayer(Direction _direction)
-    //{
-    //    switch(_direction)
-    //    {
-    //        case Direction.Right:
-    //            player.transform.DOMoveX(player.transform.position.x + moveDistance, tweenTime).SetEase(moveEase).OnComplete(() => ShakeCamera());
-    //            break;
-    //        case Direction.Left:
-    //            player.transform.DOMoveX(player.transform.position.x - moveDistance, tweenTime).SetEase(moveEase).OnComplete(() => ShakeCamera());
-    //            break;
-    //        case Direction.Up:
-    //            player.transform.DOMoveZ(player.transform.position.z + moveDistance, tweenTime).SetEase(moveEase).OnComplete(() => ShakeCamera());
-    //            break;
-    //        case Direction.Down:
-    //            player.transform.DOMoveZ(player.transform.position.z - moveDistance, tweenTime).SetEase(moveEase).OnComplete(() => ShakeCamera());
-    //            break;
+    void MovePlayer(Direction _direction)
+    {
+        switch (_direction)
+        {
+            case Direction.Right:
+                player.transform.DOMoveX(player.transform.position.x + moveDistance, tweenTime).SetEase(moveEase).OnComplete(() => ShakeCamera());
+                break;
+            case Direction.Left:
+                player.transform.DOMoveX(player.transform.position.x - moveDistance, tweenTime).SetEase(moveEase).OnComplete(() => ShakeCamera());
+                break;
+            case Direction.Up:
+                player.transform.DOMoveZ(player.transform.position.z + moveDistance, tweenTime).SetEase(moveEase).OnComplete(() => ShakeCamera());
+                break;
+            case Direction.Down:
+                player.transform.DOMoveZ(player.transform.position.z - moveDistance, tweenTime).SetEase(moveEase).OnComplete(() => ShakeCamera());
+                break;
 
-    //    }
-    //    ScalePlayer(player.transform.localScale * 2);
-    //}
+        }
+        ScalePlayer(player.transform.localScale * 2);
+    }
 
     void ScalePlayer(Vector3 _scaleTo)
     {
@@ -77,10 +77,10 @@ public class Playtime : GameBehaviour
         player.GetComponent<Renderer>().material.DOColor(GetRandomColour(), 0.5f);
     }
 
-    //void ShakeCamera()
-    //{
-    //    Camera.main.DOShakePosition(tweenTime/2, 1.2f);
-    //    _UI.TweenScore();
-    //}
+    void ShakeCamera()
+    {
+        Camera.main.DOShakePosition(tweenTime / 2, 1.2f);
+        _UI.TweenScore();
+    }
 
 }
--- Assets/Universal/Scripts/UIManager.cs
+++ Assets/Universal/Scripts/UIManager.cs
@@ -8,27 +8,27 @@ using UnityEngine.UI;
 public class UIManager : GameBehaviour<UIManager>
 {
 
-    //public TMP_Text scoreText;
-    //int score = 0;
-    //int scoreBonus = 50;
-    //public Ease scoreEase;
+    public TMP_Text scoreText;
+    int score = 0;
+    int scoreBonus = 50;
+    public Ease scoreEase;
 
     public Slider healthBarSlider;
 
     // Start is called before the first frame update
     void Start()
     {
-        //scoreText.text = score.ToString();
+        scoreText.text = score.ToString();
     }
 
-    // Update is called once per frame
-    //public void TweenScore()
-    //{
-    //    DOTween.To (() => score, x => score = x, score + scoreBonus, 1).SetEase(scoreEase).OnUpdate(() => 
-    //    {
-    //        scoreText.text = "Score: " + score.ToString();
-    //    });
-    //}
+    //Update is called once per frame
+    public void TweenScore()
+    {
+        DOTween.To(() => score, x => score = x, score + scoreBonus, 1).SetEase(scoreEase).OnUpdate(() =>
+        {
+            scoreText.text = "Score: " + score.ToString();
+        });
+    }
 
     public void SetMaxHealth(int _health)
     {
