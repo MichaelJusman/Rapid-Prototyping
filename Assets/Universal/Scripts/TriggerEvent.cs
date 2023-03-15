db73eac0c11d302051fe9ff529a32836e506b3f5
MichaelJusman <107088338+MichaelJusman@users.noreply.github.com>
2023-03-15 18:13:50 +1100

    bugs

---
+++ Assets/Universal/Scripts/TriggerEvent.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerEvent : MonoBehaviour
+{
+    public UnityEvent OnTriggerEnter;
+
+    
+    public void OnTriggerEnter(Collider other)
+    {
+
+    }
+    public void OnTriggerStay(Collider other)
+    {
+
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+
+    }
+}
