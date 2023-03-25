685a39df0d5debfd887084ef2e3aaae95ea6e785
MichaelJusman <107088338+MichaelJusman@users.noreply.github.com>
2023-03-26 00:17:52 +1100

    broken pivot controls

--- Assets/Prototype3/Script/PivotMovement1.cs
+++ Assets/Prototype3/Script/PivotMovement1.cs
@@ -2,17 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PivotMovement1 : MonoBehaviour
+public class PivotMovement1 : GameBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject neckPivot; //position of the point you want to rotate around
+    public GameObject mouthPivot; //position of the point you want to rotate around
+
+    public float rotationSpeed = 20;
+
+    private void Start()
     {
-        
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.A))
+        {
+            neckPivot.transform.RotateAround(neckPivot.transform.position, Vector3.up, rotationSpeed * Time.deltaTime);
+            //neckPivot.transform.RotateAround(neckPivot.transform.position, Vector3.up, rotationSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            neckPivot.transform.RotateAround(neckPivot.transform.position, -Vector3.up, rotationSpeed * Time.deltaTime);
+            //neckPivot.transform.RotateAround(neckPivot.transform.position, -Vector3.up, rotationSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            mouthPivot.transform.RotateAround(mouthPivot.transform.position, -Vector3.left, rotationSpeed * Time.deltaTime);
+            
+            //mouthPivot.transform.RotateAround(neckPivot.transform.position, -Vector3.left, rotationSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            mouthPivot.transform.RotateAround(mouthPivot.transform.position, Vector3.left, rotationSpeed * Time.deltaTime);
+            //mouthPivot.transform.RotateAround(neckPivot.transform.position, Vector3.left, rotationSpeed * Time.deltaTime);
+        }
+
     }
 }
